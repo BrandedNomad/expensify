24988bcef36b7dccd8e3136ed509de2791c1d6a9
BrandedNomad <info@brandednomad.com>
2020-05-18 12:48:28 +0700

    created production server.

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const server = express()
+
+const publicDirectoryPath = path.join(__dirname,'../public')
+
+server.use(express.static(publicDirectoryPath))
+
+server.get('*',(req,res)=>{
+    res.send(path.join(publicDirectoryPath,'index.html'))
+})
+
+const port = 3000
+
+server.listen(port, ()=>{
+    console.log("Server is up and running on port " + port)
+})
